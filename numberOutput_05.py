025c737530229873fdc1ee3a4852bd590e37ca4f
JasonDragon-Wang <72584791+JasonDragon-Wang@users.noreply.github.com>
2020-10-09 15:42:51 +0800

    Delete numberOutput_05.py

--- numberOutput_05.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-def numberOutput(string):
-    numcount = Counter(string)
-    print(''.join(sorted(numcount.elements())))
-def main():
-    numberStr = input("请输入一个字符串(仅含0-9的数字):")
-    numberOutput(numberStr)
-main()
